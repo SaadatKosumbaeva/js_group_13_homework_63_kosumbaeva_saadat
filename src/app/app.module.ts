51a5c4b2ae7d27ec1e39393599d76a040d98050f
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-12-10 14:48:18 +0600

    Created basic app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostItemComponent } from './posts/post-item/post-item.component';
+import { ManagePostsComponent } from './posts/manage-posts/manage-posts.component';
+import { PostDetailsComponent } from './posts/manage-posts/post-details/post-details.component';
+import { NewPostComponent } from './posts/manage-posts/new-post/new-post.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostItemComponent,
+    ManagePostsComponent,
+    PostDetailsComponent,
+    NewPostComponent,
+    AboutComponent,
+    ContactsComponent,
+    ToolbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
